55256fcd04108c958565e6e8cf963144bc2e9599
Adisa Elijah Abisola <adisaelijah2001@gmail.com>
2026-08-07 16:31:58 +0100

    question

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const startscreen = document.getElementById('start-screen');
 const startbutton = document.getElementById('start-btn');
-const quizscreen = document.getElementById('quiz screen');
+const quizscreen = document.getElementById('quiz-screen');
 const questiontext = document.getElementById('question-text');
 const currentquestion = document.getElementById('current-question');
 const scorespan = document.getElementById('score');
@@ -10,3 +10,19 @@ const resultscreen = document.getElementById('result-screen');
 const finalscore = document.getElementById('final-score');
 const maxscore = document.getElementById('max-score');
 const restartbtn = document.getElementById('restart-btn');
+const resultmessage = document.getElementById('result-message');
+const totalquestions = document.getElementById('total-questions');
+
+const quizquestions = [
+    {
+        question: 'What is the capital of france?',
+        answer: [
+            {text: 'London', correct: false},
+            {text: 'Berlin', correct: false},
+            {text: 'Paris', correct: true},
+            {text: 'Madrid', correct: false},
+        ]
+    }
+
+    
+]
